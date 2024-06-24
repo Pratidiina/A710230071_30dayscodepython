6e99fa8b7c8466d1be21af1647a5bba2bc98984b
Pratidiina <pratidiinawidya@gmail.com>
2024-06-24 07:56:24 +0700

    Create aritmatika.py

---
+++ day 16/aritmatika.py
@@ -0,0 +1,15 @@
+# Mendefinisikan variabel
+a = 10
+b = 20
+
+# Menampilkan hasil penjumlahan
+print("Penjumlahan:", a + b)
+
+# Menampilkan hasil pengurangan
+print("Pengurangan:", a - b)
+
+# Menampilkan hasil perkalian
+print("Perkalian:", a * b)
+
+# Menampilkan hasil pembagian
+print("Pembagian:", a / b)
